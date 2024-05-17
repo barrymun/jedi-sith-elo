6b74e2dce7ffe76165bb7393bbb8908609347503
Neil Murphy <barrymun@tcd.ie>
2024-05-17 12:36:04 +0100

    battler renamed to duelist

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ type Jedi struct {
 // Duel represents a duel between Jedi/Sith
 type Duel struct {
 	Title         string   `json:"title"`
-	Battler       string   `json:"battler"`
+	Duelist       string   `json:"duelist"`
 	Versus        string   `json:"versus"`
 	Location      string   `json:"location"`
 	Winner        string   `json:"winner"`
@@ -38,46 +38,46 @@ func CalculateExpectedScore(ratingA, ratingB float64) float64 {
 
 // UpdateRatings updates the ratings of two participants after a duel
 func UpdateRatings(jedis map[string]*Jedi, duel Duel) {
-	battler := jedis[duel.Battler]
+	duelist := jedis[duel.Duelist]
 	versus := jedis[duel.Versus]
 
-	expectedScoreBattler := CalculateExpectedScore(battler.Rating, versus.Rating)
-	expectedScoreVersus := CalculateExpectedScore(versus.Rating, battler.Rating)
+	expectedScoreBattler := CalculateExpectedScore(duelist.Rating, versus.Rating)
+	expectedScoreVersus := CalculateExpectedScore(versus.Rating, duelist.Rating)
 
 	// If it's a single duel
 	if !duel.IsMulti {
-		if duel.Winner == battler.Name {
-			battler.Rating += K * (1 - expectedScoreBattler)
+		if duel.Winner == duelist.Name {
+			duelist.Rating += K * (1 - expectedScoreBattler)
 			versus.Rating += K * (0 - expectedScoreVersus)
 		} else if duel.Winner == versus.Name {
-			battler.Rating += K * (0 - expectedScoreBattler)
+			duelist.Rating += K * (0 - expectedScoreBattler)
 			versus.Rating += K * (1 - expectedScoreVersus)
 		}
 		return
 	}
 
 	// Multi duel adjustments
-	if duel.Winner == battler.Name {
-		if contains(duel.MultiDuelists, battler.Name) {
-			battler.Rating += K * (1 - expectedScoreBattler) * 0.75
+	if duel.Winner == duelist.Name {
+		if contains(duel.MultiDuelists, duelist.Name) {
+			duelist.Rating += K * (1 - expectedScoreBattler) * 0.75
 			versus.Rating += K * (0 - expectedScoreVersus) * 1.25
 		} else {
-			battler.Rating += K * (1 - expectedScoreBattler) * 1.25
+			duelist.Rating += K * (1 - expectedScoreBattler) * 1.25
 			versus.Rating += K * (0 - expectedScoreVersus) * 0.75
 		}
 	} else if duel.Winner == versus.Name {
 		if contains(duel.MultiDuelists, versus.Name) {
 			versus.Rating += K * (1 - expectedScoreVersus) * 0.75
-			battler.Rating += K * (0 - expectedScoreBattler) * 1.25
+			duelist.Rating += K * (0 - expectedScoreBattler) * 1.25
 		} else {
 			versus.Rating += K * (1 - expectedScoreVersus) * 1.25
-			battler.Rating += K * (0 - expectedScoreBattler) * 0.75
+			duelist.Rating += K * (0 - expectedScoreBattler) * 0.75
 		}
 	} else { // Draw
-		if contains(duel.MultiDuelists, battler.Name) {
-			battler.Rating -= K * 0.1
+		if contains(duel.MultiDuelists, duelist.Name) {
+			duelist.Rating -= K * 0.1
 		} else {
-			battler.Rating += K * 0.1
+			duelist.Rating += K * 0.1
 		}
 		if contains(duel.MultiDuelists, versus.Name) {
 			versus.Rating -= K * 0.1
@@ -95,6 +95,46 @@ func contains(slice []string, item string) bool {
 		}
 	}
 	return false
+}
+
+// Load duels from a JSON file
+func loadDuels(filename string) ([]Duel, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	byteValue, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+
+	var duels []Duel
+	err = json.Unmarshal(byteValue, &duels)
+	if err != nil {
+		return nil, err
+	}
+
+	return duels, nil
+}
+
+// Get unique names from the duels
+func getUniqueNames(duels []Duel) []string {
+	nameSet := make(map[string]struct{})
+	for _, duel := range duels {
+		nameSet[duel.Duelist] = struct{}{}
+		nameSet[duel.Versus] = struct{}{}
+		for _, duelist := range duel.MultiDuelists {
+			nameSet[duelist] = struct{}{}
+		}
+	}
+
+	names := make([]string, 0, len(nameSet))
+	for name := range nameSet {
+		names = append(names, name)
+	}
+	return names
 }
 
 func main() {
@@ -133,44 +173,4 @@ func main() {
 	for _, jedi := range jediList {
 		fmt.Printf("%s: %.2f\n", jedi.Name, jedi.Rating)
 	}
-}
-
-// Load duels from a JSON file
-func loadDuels(filename string) ([]Duel, error) {
-	file, err := os.Open(filename)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	byteValue, err := ioutil.ReadAll(file)
-	if err != nil {
-		return nil, err
-	}
-
-	var duels []Duel
-	err = json.Unmarshal(byteValue, &duels)
-	if err != nil {
-		return nil, err
-	}
-
-	return duels, nil
-}
-
-// Get unique names from the duels
-func getUniqueNames(duels []Duel) []string {
-	nameSet := make(map[string]struct{})
-	for _, duel := range duels {
-		nameSet[duel.Battler] = struct{}{}
-		nameSet[duel.Versus] = struct{}{}
-		for _, duelist := range duel.MultiDuelists {
-			nameSet[duelist] = struct{}{}
-		}
-	}
-
-	names := make([]string, 0, len(nameSet))
-	for name := range nameSet {
-		names = append(names, name)
-	}
-	return names
 }
